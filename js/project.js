0d95402ed7e7ab251466b244b668a70440a34387
Crista Chapman <cristachapman@gmail.com>
2017-07-07 13:25:42 -0400

    Rough draft with js plugin

--- js/project.js
+++ js/project.js
@@ -1,10 +1,7 @@
 // PORTFOLIO JS STARTS HERE
-$(".thumb").on("mouseenter", function() {
-	var image = $(this).attr("src");
-	$("#bigimage").hide();
-	$("#bigimage").attr("src", image);
-	$("#bigimage").fadeIn(500);
-});
+$(".thumbnail").simpleLightbox();
+
+
 
 // Working on a function that tells the #bigimage to find the corresponding
 // anchor so that when the viewer clicks on the big image, it links
