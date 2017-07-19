4ecc6d5152fefcdca63af64753cc75b073de1f32
danaliss <danaliss@gmail.com>
2017-07-19 18:16:15 -0400

    Presentation 07192017

--- js/project.js
+++ js/project.js
@@ -1,34 +1,28 @@
-// $("#menu").on("click", function(){
-// 	$("nav").slideToggle(400);
-//   	/
-// });
-
 $(document).ready(function(){
   $(".projects").click(function(){
-    // The long way
-    // var wrapper = $(this).find(".wrapper");
-    // if (wrapper.css("display") == "none"){
-    //   wrapper.slideDown(400);
-    // }
-    // else {
-    //   wrapper.slideUp(400);
-    // }
-    // var icon = $(this).find("span");
-    // if(icon.hasClass("fa-chevron-down")) {
-    //   icon.removeClass("fa-chevron-down");
-    //   icon.addClass("fa-chevron-up");
-    // }
-    // else {
-    //   icon.removeClass("fa-chevron-up");
-    //   icon.addClass("fa-chevron-down");
-    // }
-    // var row = $(this); // ".row"
- 
-    // The better way!
-    $(this).find(".accordion").slideToggle(400, function(){
-      $(this).css("display", "block");
-      // $(this).parent(); // ".row"
+
+  	var project = $(this);
+
+    project.find(".accordion").slideToggle(700, function(){
+
+    	// console.log(project.find("span").html());
+
+    	project.toggleClass("open");
+
+    	if (project.hasClass("open")) {
+			project.find("span").html(" &#8722;");
+		}
+		else {
+			project.find("span").html(" &#43;");
+		}
+
+  //   	if (project.find("span").html() == " &#43;") {
+		// 	project.find("span").html(" &#8722;");
+		// }
+		// else {
+		// 	project.find("span").html(" &#43;");
+		// }
+		
     });
-  //   $(this).find("span").toggleClass("fa-chevron-down fa-chevron-up");
-  // });
+  });
 });
